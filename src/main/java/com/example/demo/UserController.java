846693db4bb7daaebc19a8b31b146a49810820f3
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-08-05 13:29:06 +0700

    Step 02

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,23 +1,25 @@
 package com.example.demo;
 
-import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class UserController {
 
+//    @GetMapping("/users")
+//    public List<UserResponse> getAllUser() {
+//        List<UserResponse> users = new ArrayList<>();
+//        users.add(new UserResponse(1, "User 1"));
+//        users.add(new UserResponse(2, "User 2"));
+//        users.add(new UserResponse(3, "User 3"));
+//        return users;
+//    }
+    // Test req param
     @GetMapping("/users")
-    public List<UserResponse> getAllUser() {
-        List<UserResponse> users = new ArrayList<>();
-        users.add(new UserResponse(1, "User 1"));
-        users.add(new UserResponse(2, "User 2"));
-        users.add(new UserResponse(3, "User 3"));
-        return users;
+    public String getPage (@RequestParam(required = false, defaultValue = "1")int page, @RequestParam(required = false, defaultValue = "10")int itemPerPage) {
+        return "Page: " +page+ ", itemPerPage: " +itemPerPage;
     }
 
     @GetMapping("/users/{id}")
