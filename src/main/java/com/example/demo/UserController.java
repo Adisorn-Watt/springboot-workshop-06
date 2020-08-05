62f9214f57b06c1c19606b4ab491439658c2ec72
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-08-05 11:43:07 +0700

    Step get user by ID by add path var

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,17 +2,27 @@ package com.example.demo;
 
 import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users")
-    public UserResponse[] getAllUser() {
-        UserResponse[] u = new UserResponse[2];
-        u[0] = new UserResponse(1, "User 1");
-        u[1] = new UserResponse(2, "User 2");
-        return u;
+    public List<UserResponse> getAllUser() {
+        List<UserResponse> users = new ArrayList<>();
+        users.add(new UserResponse(1, "User 1"));
+        users.add(new UserResponse(2, "User 2"));
+        users.add(new UserResponse(3, "User 3"));
+        return users;
+    }
+
+    @GetMapping("/users/{id}")
+    public UserResponse getUserById(@PathVariable int id) {
+        return new UserResponse(id, "User " + id);
     }
 
 }
