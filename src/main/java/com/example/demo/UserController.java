6aa8b081e1703ab1bf3a2abdc128dc2307d76892
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-08-05 11:09:57 +0700

    Step 01

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/users")
-    public String getAllUser() {
-         return "Get All User";
+    public UserResponse[] getAllUser() {
+        UserResponse[] u = new UserResponse[2];
+        u[0] = new UserResponse(1, "User 1");
+        u[1] = new UserResponse(2, "User 2");
+        return u;
     }
 
 }
